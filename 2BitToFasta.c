52f432368d0cc78937046184b24ac2dc05d1e45c
Kyle and Robert <rrderber@gmail.com>
2016-05-31 12:21:44 -0500

    2BitToFasta completed.

--- 2BitToFasta.c
+++ 2BitToFasta.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int bitSeparator(char * byte){
-int i;
+int bitSeparator(char byt, FILE * ofp){
+		int i;
                         for(i = 3; i>0; --i){
                                 char temp = byt & ('\x03' << (2*i));//get nucleotide from byte
                                 temp =  (temp >> (i*2)-1)&'\x06';//shift to position in ascii char and make sure no ones were carried
@@ -24,8 +24,8 @@ int i;
                         }
                         fprintf(ofp, "%c", byt);
 //                      fprintf(ofp, "%c", '\n');
-
-}
+		return 0;	
+		}
 
 
 
@@ -50,41 +50,56 @@ int twoBit(const char * filename,const char * outFileName){
 	while(!eof){
 		byt = getc(ifp);
 		if((~ byt)){//check if eof or all Gs
-			int i;
-			for(i = 3; i>0; --i){
-				char temp = byt & ('\x03' << (2*i));//get nucleotide from byte
-				temp =  (temp >> (i*2)-1)&'\x06';//shift to position in ascii char and make sure no ones were carried
-				if(!(temp^'\x04')){//if temp is a T, not A,G,C
-					temp = 'T';
-				}
-				else{
-					temp = temp | '\x41';	//convert to ascii char
-				}
-				fprintf(ofp, "%c", temp);
-			}
-			byt = byt & '\x03';//get last nucleotide
-			byt = byt << 1;//shift to position in ascii char
-			if(!(byt^'\x04')){//if temp is a T, not A,G,C
-				byt = 'T';
-			}
-			else{
-				byt = byt | '\x41';	//convert to ascii char
-			}
-			fprintf(ofp, "%c", byt);
+
+
+	bitSeparator(byt, ofp); // helped function for commented-out code below
+
+
+
+
+
+//			int i;
+//			for(i = 3; i>0; --i){
+//				char temp = byt & ('\x03' << (2*i));//get nucleotide from byte
+//				temp =  (temp >> (i*2)-1)&'\x06';//shift to position in ascii char and make sure no ones were carried
+//				if(!(temp^'\x04')){//if temp is a T, not A,G,C
+//					temp = 'T';
+//				}
+//				else{
+//					temp = temp | '\x41';	//convert to ascii char
+//				}
+//				fprintf(ofp, "%c", temp);
+//			}
+//			byt = byt & '\x03';//get last nucleotide
+//			byt = byt << 1;//shift to position in ascii char
+//			if(!(byt^'\x04')){//if temp is a T, not A,G,C
+//				byt = 'T';
+//			}
+//			else{
+//				byt = byt | '\x41';	//convert to ascii char
+//			}
+//			fprintf(ofp, "%c", byt);
 //			fprintf(ofp, "%c", '\n');
 		}else{
-			int i;
+			int i=1;
 			char checkeof;
 			char eofarr[30];
+			eofarr[0] = byt;
 			while(!((checkeof=getc(ifp)) ^ '\xff') && i < 30){//extract up to 30 bytes of -1
 			//	checkeof = getc(ifp);
 				eofarr[i]=checkeof;
 				++i;
 			}
+
 			if(i!=30){
-				for(i = i-1; i=0;--i){
-					ungetc(eofarr[i],ifp);	//if a non -1 byte was extracted undo extraction of last i bytes
+				int k=0;
+				for(k;k<i;++k){
+				//	ungetc(eofarr[i],ifp);	//if a non -1 byte was extracted undo extraction of last i bytes
+				
+				bitSeparator(eofarr[k],ofp);
+					
 				}
+				bitSeparator(checkeof, ofp);
 			}else{
 				eof = 1;//if 30 -1 bytes were extracted end of file is true
 			}
