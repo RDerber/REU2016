f7e0e43fb0589004680b9abd68274ad07207c099
Kyle and Robert <rrderber@gmail.com>
2016-06-06 15:40:55 -0500

    Evaluate Function fixed, but Overflow still occurs with 11 terms taken in.

--- LaGrangeGen.c
+++ LaGrangeGen.c
@@ -25,37 +25,46 @@
 
 
 
-int* polyMult(int points[], long long prod[], int numPoints){ //Takes in an array of points (with one already removed) and the size of this array
+int* polyMult(int points[], long long prod[], int numPoints, long long mod){ //Takes in an array of points (with one already removed) and the size of this array
 							//and modifies prod[] so it contains the coefficients of the numerators of each line
 	int i;
+	int k;
+	memset(prod,0,((numPoints+1)*sizeof(prod[0])));
+//	printf("%s %c","PolyMult: ",'\n');
+//	printf("%s %lld \n","Mod: ",mod);
+
 	prod[0] = 1;
-	prod[1] = points[0] + points[1];
-	prod[2] = points[0]*points[1];
-//	printf("%d %c",numPoints, '\n');
+	prod[1] = (points[0] + points[1]) % mod;
+	prod[2] = (points[0]*points[1]) % mod;
+	printf("%d %c",numPoints, '\n');
 	for(i = 2; i< numPoints; ++i){
 		int j;
 		long long prodTemp[numPoints+1];
-		int k;
+		memset(prodTemp,0,sizeof(prodTemp));
+
 		for(k=0;k<numPoints+1;++k){
 			prodTemp[k]=prod[k];
 		}
 		for(j = 1;j <= i;++j){
 			long long last = prod[i];
 			if(j==1){
-				prod[j] = prod[j] + points[i];
+				prod[j] = (prod[j] + points[i]);
                         }else {
 				if(j==i){
                                 	prod[i+1] = last;
 				}
-				prod[j] = prodTemp[j-1]*points[i] + prodTemp[j];
+				prod[j] = ((prodTemp[j-1]*points[i]) + prodTemp[j]);
 			}
 		}
-		prod[i+1] *= points[i];
+		prod[i+1] = (prod[i+1] * points[i]);
 //		for(k=0;k<numPoints+1;++k){
-//			printf("%d",prod[k]);
+//			printf("%s %d %s", "Coefficient of X^",k,": ");
+//			printf("%lli",prod[k]);
 //			printf("%c",'\n');
 //		}
 	}
+
+	printf("%s %c", "End PolyMult.", '\n');
 	return 0;
 }
 
@@ -66,7 +75,7 @@ long long denom(int x, int tempPoints[], int numPoints){ // Takes in the removed
 	long long temp;
 	for(i = 0; i < numPoints; ++i){
 		temp = -x+tempPoints[i];
-		den *= temp;
+		den = (den * temp) % 225735769;//modded by the product of mersenne primes to maintain information but avoid overflow
 	}
 //	printf("%d %c", den, '\n');
 	return den;
@@ -74,23 +83,27 @@ long long denom(int x, int tempPoints[], int numPoints){ // Takes in the removed
 
 
 
-int findMod(long long denoms [], int size){ // Takes in all denominators and finds a mersenne prime that is not a factor of any of them
+int findMod(long long denoms [], int size, int maxY){ // Takes in all denominators and finds a mersenne prime that is not a factor of any of them
 	int mersenne[] = {7,31,127,8191};
 	int i;
 	int j;
 	int count;
-
 	for(i=0; i<4;++i){
- 		count = 0;
-		for(j=0; j<size; ++j){
-			if((denoms[j]%mersenne[i])!= 0){
-				++count; 
-			}
-	//	printf("%d %c %d %c %d %c", denoms[j],'\n', denoms[j]%mersenne[i], '\n', count, '\n');
+//		printf( "%d %c", maxY, '\n');
+		if(mersenne[i] <= maxY){
 		}
-		if(count==size){
-	//		printf("%d %c", mersenne[i], '\n');
-			return mersenne[i];
+		else{
+ 		count = 0;
+			for(j=0; j<size; ++j){
+				if((denoms[j]%mersenne[i])!= 0){
+					++count; 
+				}	
+	//	printf("%d %c %d %c %d %c", denoms[j],'\n', denoms[j]%mersenne[i], '\n', count, '\n');
+			}
+				if(count==size){
+	//				printf("%d %c", mersenne[i], '\n');
+					return mersenne[i];
+				}
 		}
 	}
 
@@ -118,7 +131,7 @@ int modInverse(long long num, int mod){ // Takes in a number A  and a modulus nu
 				  // So that (inversMod * A) mod B =1 
 	int inverse = 1;
 	num = num % mod; // a value in Flt
-	printf("%s %d %c", "num: ", num, '\n');
+//	printf("%s %d %c", "num: ", num, '\n');
 	int modm2= mod - 2;
 	int mult = (num+mod) % mod;
 //	printf("%s %d %c", "mult: ", mult, '\n');
@@ -150,21 +163,30 @@ int evaluate(int x, int polys[], int polysize, int mod){ // UNDER CONSTRUCTION -
 	int i;
 	int sum = 0;
 	int polyTable[polysize]; // Stores all X's with power of 2 exponent 
+	memset(polyTable, 0 , sizeof(polyTable));
 	polyTable[0] = x;
-//	printf("%s %d %c", "size of poly: ", polysize, '\n');
+	int check = 1;
+	int log = 0;
+	printf("%s %d %c", "size of poly: ", polysize, '\n');
 	for(i = 0; i < polysize; ++i){
-		int poly = (polysize - i) - 1;
+		int poly = i;
 		int j;
 		int product = 1;
-		if(i>0){
-			polyTable[i] = (polyTable[i-1]*polyTable[i-1])%mod;
+		printf("%s %d \n", "check: ", check);
+		if(i & check){
+			++log;
+			polyTable[log] = (polyTable[log-1]*polyTable[log-1])%mod;
+			check <<= 1;
 		}
-		for(j = 0; j < i; ++j){
+		for(j = 0; j < log; ++j){
+			printf("%s %d \n", "polyTable: ", polyTable[j]);
 			if(((poly) >> j) & '\x01'){
 				product = (product * polyTable[j]) % mod;
 			}
 		}
-		sum = (sum + (product * polys[i]) % mod) % mod;
+		printf(" %s \n", "-------");
+		sum = (sum + (product * (polys[polysize - i - 1] % mod)) % mod) % mod;
+//		printf("%s %d %s %d %s %d %s %d \n","Product: ",product,", Sum: ",sum, "polys: ", polys[polysize-i-1],"polyTable: ", polyTable[i]);
 	}
 	sum = (sum + mod) % mod;
 	return sum;
@@ -177,7 +199,7 @@ int polyGenerator(int points[], int yValues[], int size, int poly[]){ // Takes i
 	long long denoms[size]; // keeps track of denomenator of each row
 	memset(denoms, 0, sizeof(denoms)); // Initializes arrays to all 0s in memory 
 	memset(poly, 0, sizeof(poly));
-
+	long long mod = 225735769;
 	for(i=0; i<size; ++i){
 		int j;
 		int pointsTemp[size-1];// size of pointsTemp is one less than number of Points as we remove one point during each iteration
@@ -187,7 +209,7 @@ int polyGenerator(int points[], int yValues[], int size, int poly[]){ // Takes i
 	//		printf("%d %s", pointsTemp[j-1],", "); // pointsTemp is working
 		}
 	//	printf("%c", '\n');
-		polyMult(pointsTemp,polyTemp[i], size-1);
+		polyMult(pointsTemp,polyTemp[i], size-1, mod);
 	
 	//	for(j=0;j<size;++j){      			// polyMult is working
 	//		printf("%d %s", polyTemp[i][j],", ");
@@ -202,14 +224,20 @@ int polyGenerator(int points[], int yValues[], int size, int poly[]){ // Takes i
 //	for(i=0; i < 2; ++i){
 //		printf("%d %c %d %c", denoms[i],'\n', denoms[i]%7, '\n');
 //	}
-//
-	int mod = findMod(denoms, size);
+
+	int maxY = 0;
+	for(i=0; i<size; ++i){
+		if(yValues[i]>maxY){
+			maxY=yValues[i];
+		}
+	}
+	mod = findMod(denoms, size, maxY);
 //	printf("%d %c", mod, '\n');
 	
 	int j;
 	for(i=0; i<size;++i){
 		int modin = modInverse(denoms[i], mod);
-		printf("%d %c", modin, '\n');
+//		printf("%d %c", modin, '\n');
 		for(j=0; j < size; ++j){
 			polyTemp[i][j] = (polyTemp[i][j] * modin) % mod;
 			poly[j]= ((yValues[i] * polyTemp[i][j])%mod + poly[j])%mod;
@@ -240,9 +268,10 @@ int main (int argc, char *argv[]){
 	int size = (argc - 3)/2;
 	int points[size];
 	int yValues[size];
+	memset(yValues,0,sizeof(yValues));
 //	int size = sizeof(points)/sizeof(points[0]);
-	printf("%s %d", "Size: ", size);
-	printf("%s %d", "ArgC: ", argc);
+//	printf("%s %d", "Size: ", size);
+//	printf("%s %d", "ArgC: ", argc);
 
 	int i;
 	for(i = 0; i < size; ++i){
@@ -261,14 +290,17 @@ int main (int argc, char *argv[]){
 		printf("%d %s", poly[i],", ");
 	}
 
-	int ch;
+	int ch=0;
+	char temp = 0;
 	while((ch = getc(ifp)) != EOF){
-		char temp = evaluate(ch, poly, size, mod);
+		temp = evaluate(ch, poly, size, mod);
+		printf("%s %d %s %d \n","Ch: ", ch, ", temp: ",temp);
 		fprintf(ofp, "%d",temp);
 	}
 	int ans = 0;
-	ans = evaluate(13, poly, size, mod);
-	printf("%c %d %c", '\n', ans, '\n');
+	int eval = 67;
+	ans = evaluate(eval, poly, size, mod);
+	printf("%c %s %d %c %d %c", '\n',"Evaluated at: ",eval,'\n', ans, '\n');
 
 	fclose(ofp);
 	fclose(ifp);
