9d56061a3d9cf581576a3a3a802358d00fd67789
Kyle and Robert <rrderber@gmail.com>
2016-06-09 14:38:51 -0500

    Begin timing changes to FastaTo2Bit.c

--- DNA Sequence Transformations/FastaTo2Bit.c
+++ DNA Sequence Transformations/FastaTo2Bit.c
@@ -30,12 +30,12 @@ int twoBit(const char * filename,const char * outFileName){
 	fprintf(ofp, "%c", '\n');
 	fflush(ofp);
 	char byt;
-	while(!((byt = getc(ifp))&'\x80')){//get first char and shift right
+	while((byt = getc(ifp))!= EOF){//get first char and shift right
 //	if(byt != '\n' && byt != '\r'){ // mask with x40
 		while(!(byt&'\x40')){
 			byt = getc(ifp);
 		}
-		if(byt&'\x80') break;
+		if(byt== EOF) break;
 		byt = byt & '\x06';
 		byt = byt << 5;
 		char temp;
@@ -68,8 +68,66 @@ int twoBit(const char * filename,const char * outFileName){
 
 int main(int argc, char *argv[]){
 	if(argc == 3){
+
+		// Define Input Memory Buffer
+	char *input = NULL;
+	File *fp = fopen(argv[1],"r");
+
+	if(fp != NULL){
+	 /* Go to the end of the file */
+	if(fseek(fp, 0L, SEEK_END)== 0){
+		/*Get the size of the file. */
+
+		long bufsize = ftell(fp);
+		if (bufsize == -1) {
+			fputs("Error finding size of file", stderr);
+		 }
+	
+		/*Allocate our buffer of that size +1 for null termination. */
+		input = malloc (sizeof(char) * (bufsize+1));
+		
+
+		/* Return to start of file*/
+		if(fseek(fp, 0L, SEEK_SET)!=0 ) {
+			fputs("Error returning to start of file", stderr);
+		}
+
+		/*Read the entire file into memory*/
+		size_t newLen = fread(input, sizeof(char), bufsize, fp);
+		if(newLen == 0){
+			fputs("Error reading file", stderr);
+		} else {
+			input[newLen++] = '\0'; // Null termination character
+			}
+		}			
+		fclose(fp);
+	}
+
+		// Define Output Buffer
+
+
+
+
+		 // do this at the end: free(input);
+
+
+		// Read in all data from argv[1] and store in input memory buffer
+
+
+		// In a for loop, run the twoBit Funtion using a timer function, reading from input memory buffer and writting to output 
+		// memory buffer. Store the run times in an array
+	
+		
 		twoBit(argv[1],argv[2]);
+
+		// Record fastest times from the 100 runs
+		// Write from output memory buffer to an output file
+		// Check for correctness
+		//
 		return 0;
+
+
+
         }else{
 		 return 1;
 	}
