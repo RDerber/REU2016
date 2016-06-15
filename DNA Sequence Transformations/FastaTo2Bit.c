a7d6279f26dc842d335d2aadf977fe3734e06090
Kyle Cepeda <kpcepeda7@gmail.com>
2016-06-15 22:07:08 +0000

    Comments updated and small error fixed in FastaTo2Bit.c

--- DNA Sequence Transformations/FastaTo2Bit.c
+++ DNA Sequence Transformations/FastaTo2Bit.c
@@ -2,11 +2,27 @@
  * FastaTo2Bit.c
  *
  * Functional for regular FASTA
+ *	Includes an optional timing report exported in JSON format
+ *  
+ * Parameters: [Input FASTA File Name] [Output 2Bit File Name] [int number of runs] [int number of minimum timing values to retain]
  *
- * we now get time, need to add for loop with minheap to get minimum times.
+ * Number of runs and min timing values are optional
+ *		 Number of runs must be provided to recieve a timing report
+ *		 The default number of min timing values is 3 if unsepecified.
  *
- * 
+ * Each nucleotide base will be converted to a two bit character:
+ *		A = 00
+ *		C = 01
+ *		G = 11
+ *		T = 10
+ *
+ * The bases are then stored in bytes, retaining the left-to-right order in which they were read
+ *  Ex: ACGT will convert to 0001 1110 
+ *	
+ * If the number of bases is not a multiple of 4, the last byte in the translation will end with (num bases % 4) pairs of zeros
+ * 	These pairs of zeros will then translate back into FASTA as extra A's appended on the end of the sequence 
  */
+ 
 
 
 
@@ -15,35 +31,42 @@
 #include <sys/time.h>
 #include <float.h>
 #include "writeJson.h"
-
-int twoBit(char * input, char * output, long inputsize){
+/*
+* Reads in A,C,G,T characters from input buffer, converts to 00, 01, 11, and 10 respectively, and writes to output buffer
+*@param input buffer
+*@param output buffer
+*@param size of input buffer
+*@return size of the output buffer
+*/
+int twoBit(char * input, char * output, long inputsize){ 
+	
+	// Move header line into output buffer // 
 	size_t firstlnlen;
-	for(firstlnlen = 0; input[firstlnlen] != '\n'; ++firstlnlen){
+	for(firstlnlen = 0; input[firstlnlen] != '\n'; ++firstlnlen){ 
 		output[firstlnlen] = input[firstlnlen];
-//		printf("%c",input[firstlnlen]);
 	}
 	output[firstlnlen]='\n';
-	long i = firstlnlen + 1;
-//	printf("\n%c\n",input[i-1]);
-	long k = i-1;
-//	printf("%s %d %s %d \n","firstlnlen:",firstlnlen,"inputsize:",inputsize);
+	
+	
+	long i = firstlnlen + 1; // input buffer index
+	long k = i-1;				// output buffer index
+	
+	// Read from input buffer, translate, and write to output buffer // 
 	while(i < inputsize){
-//	printf("%s %d %s %c \n","i is:", i, "input[i] is:",input[i]);
-//	printf("%d \n",i);
-//	printf("%s %c \n","Input[i]:",input[i]);
-		while(i<inputsize && !(input[i]&'\x40')){
-			++i;
+
+		while(i<inputsize && !(input[i]&'\x40')){ // Check for buffer overflow 
+			++i;												// If character is not a desired letter, increase to the next character in the buffer
 		}
-		if(i >= inputsize) break;
+		if(i >= inputsize) break;						// If index increases past buffer size, stop reading
 		char byt = input[i];
 		++k;
 		++i;
-		byt = byt &'\x06';
-		byt = byt << 5;
+		byt = byt &'\x06';								// 2 Bit conversion of first base in grouping of 4
+		byt = byt << 5;									// Shifted to the left-most position on the output byte
 		char temp;
 		unsigned j;
-		for(j = 2; j > 0 && i < inputsize; --j){
-//			printf("%s %d \n","i in for loop:",i);
+		for(j = 2; j > 0 && i < inputsize; --j){ // 2 Bit conversion of the 2nd and 3rd bases
+
 			if(input[i] & '\x40'){
 				temp = input[i];
 				++i;
@@ -55,57 +78,57 @@ int twoBit(char * input, char * output, long inputsize){
 				++i;
 			}
 		}
-		while(i < inputsize && !(input[i] & '\x40')){
+		while(i < inputsize && !(input[i] & '\x40')){ //Checking for buffer overflow and desired character
 			++i;
 		}
 
 			
-		if(i < inputsize){
+		if(i < inputsize){									// 2 Bit conversion of the last base in a grouping of 4
 			temp = input[i];
 			++i;
 			temp = temp & '\x06';
 			temp = temp >> 1;
 			byt = byt|temp;
 		}
-//	printf("\n%d\n", k);
+
 	output[k] = byt;
-//	printf("%s %d \n","i is:",i);
+
 	}
-	return k+1;
+	return k+1; // returns the size of the output array 
 }
 
 int main(int argc, char *argv[]){
-	if(argc == 3||argc == 4||argc == 5){
+	if(argc == 3||argc == 4||argc == 5){ 
 
-		// Define Input Memory Buffer
+	// Create Input Memory Buffer //
 	char *input = NULL;
 	FILE *ifp = fopen(argv[1],"r");
 	long inputsize = 0;
 	if(ifp != NULL){
-	 /* Go to the end of the file */
+	 // Go to the end of the file //
 	if(fseek(ifp, 0L, SEEK_END)== 0){
-		/*Get the size of the file. */
+		// Get the size of the file. //
 
 		inputsize = ftell(ifp);
 		if (inputsize == -1) {
 			fputs("Error finding size of file", stderr);
 		 }
 	
-		/*Allocate our buffer of that size +1 for null termination. */
+		//Allocate our buffer of that size +1 for null termination. //
 		input = malloc (sizeof(char) * (inputsize+1));
 		
 
-		/* Return to start of file*/
+		// Return to start of file //
 		if(fseek(ifp, 0L, SEEK_SET)!=0 ) {
 			fputs("Error returning to start of file", stderr);
 		}
 
-		/*Read the entire file into memory*/
+		//Read the entire file into memory//
 		size_t newLen = fread(input, sizeof(char), inputsize, ifp);
 		if(newLen == 0){
 			fputs("Error reading file", stderr);
 		} else {
-			input[newLen++] = '\0'; // Null termination character
+			input[newLen++] = '\0'; // Null termination character at the end of the input buffer 
 			}
 		}			
 		fclose(ifp);
@@ -114,93 +137,52 @@ int main(int argc, char *argv[]){
 		return 1;
 	}
 
-		// Define Output Buffer;
+		// Create Output Buffer;
 	char * output = malloc(sizeof(char)* (inputsize+1));
 	int outputsize = 0;
-		// Read in all data from argv[1] and store in input memory buffer
-
-
-//	int k;
-//	for(k=0; k<inputsize; ++k){
-//	printf("%s %c \n","Input[k]:", input[k]);
-//	}
-		// In a for loop, run the twoBit Funtion using a timer function, reading from input memory buffer and writting to output 
-		// memory buffer. Store the run times in an array
+	
 	float *times;
-//	float *minTimes;
 	int runs = 0;
 	int numTimes = 0;
+	if(argc == 3){
+		outputsize = twoBit(input,output,inputsize);
+	}
 	if(argc == 4){	//if a number of runs is given but no number of minimum times, default number of min times is 3
 		runs = atoi(argv[3]);
 		numTimes = 3;
 		times = calloc(runs, sizeof(float)); 
-//		minTimes = calloc(numTimes,sizeof(float));
 		struct timeval time0, time1; 
 		int i;
-		for(i=0;i<runs;i++){
+		for(i=0;i<runs;i++){ // Record time of each run
 			gettimeofday(&time0,NULL);
 			outputsize = twoBit(input,output,inputsize);
 			gettimeofday(&time1,NULL);
 			times[i] = (time1.tv_sec-time0.tv_sec)*1000000LL + time1.tv_usec - time0.tv_usec;
 		}
-//		int k;
-//		for(k=0;k < numTimes;++k){
-//			float minTime = times[0];
-//			int index = 0;
-//			for(i=0;i < runs;++i){
-//				if(times[i] < minTime){
-//					minTime = times[i];
-//					index = i;
-//				}
-//			minTimes[k] = minTime;
-//			times[index] = 	FLT_MAX;
-//			}
-//		}
+
 	}
 	if(argc == 5){ //if both number of runs and the number of minimum times is given
 		runs = atoi(argv[3]);
                 numTimes = atoi(argv[4]);
                 times = calloc(runs, sizeof(float));
-//              minTimes = calloc(numTimes,sizeof(float));
-                struct timeval time0, time1;
+                struct timeval time0, time1; 
                 int i;
-                for(i=0;i<runs;i++){
+                for(i=0;i<runs;i++){ // Record time of each run
                         gettimeofday(&time0,NULL);
                         outputsize = twoBit(input,output,inputsize);
                         gettimeofday(&time1,NULL);
                         times[i] = (time1.tv_sec-time0.tv_sec)*1000000LL + time1.tv_usec - time0.tv_usec;
                 }
-//              int k;
-//              for(k=0;k < numTimes;++k){
-//                      float minTime = times[0];
-//                      int index = 0;
-//                      for(i=0;i < runs;++i){
-//                              if(times[i] < minTime){
-//                                      minTime = times[i];
-//                                      index = i;
-//                              }
-//                      minTimes[k] = minTime;
-//                      times[index] =  FLT_MAX;
-//                      }
-//              }	
-	}
-//	int i;
-//	float aveTime = 0;
-//	for( i = 0; i < numTimes; ++i){
-//		aveTime += minTimes[i];
-//	}
-//	free(minTimes);
-//	aveTime /= (float)numTimes;
-	
-	// Include JSON file formatting code and use it to print out report of data in JSON Output file
-	//
-	
 
+	}
+
+	// JSON timing.txt file output if [runs] and [num min times] arguments are included // 
 	if(argc > 3 && write_time_file(times, runs, numTimes,inputsize) < 0)
 		printf("error writing time file\n");
 	
 	free(times);
 
+	// Writing output buffer to specified output file//
 	FILE *ofp = fopen(argv[2],"w");
 	int i;
 	for(i=0; i<outputsize; ++i){
