0b2fed415820e3c74f91216b04c3ab53c30dd8e5
jasondzheng <jasondzheng@gmail.com>
2016-06-16 13:54:27 -0500

    FileReader2 working

--- Character Trajectories/FileReader2.c
+++ Character Trajectories/FileReader2.c
@@ -14,9 +14,10 @@
 //TIFF HEADER CODES
 #define HEADER_ENDIAN_LITTLE            0x4949
 #define HEADER_ENDIAN_BIG               0x4D4D
-#define HEADER_MAGIC_NUMBER             0x002a
+#define HEADER_MAGIC_NUMBER             0x002A
 
 #define NUM_TAGS                            11
+#define TIFF_END                    0x00000000
 
 //TIFF TAG ID CODES                           
 #define TAG_WIDTH                       0x0100
@@ -29,7 +30,7 @@
 #define TAG_STRIP_BYTE_COUNTS           0x0117
 #define TAG_X_RESOLUTION                0x011a
 #define TAG_Y_RESOLUTION                0x011b
-#define TAG_RESOLUTION_UNIT             0x0152
+#define TAG_RESOLUTION_UNIT             0x0153
 
 //Loads the byte data of the .idx3-ubyte at the given filepath into the 
 //given buffer and returns the length of the file(buffer)
@@ -85,7 +86,7 @@ unsigned int idx3ubyte_read_2byte_int(unsigned char *inputBuffer,
 struct header {
 		uint16_t endian;
 		uint16_t magicNumber;
-		uint16_t ifdOffset;
+		uint32_t ifdOffset;
 };
 
 struct tag {
@@ -103,37 +104,72 @@ struct header generate_tiff_header(unsigned int width, unsigned int height){
 	return tiffHeader;
 }
 
-struct tag * generate_tiff_tags(unsigned int width, unsigned int height){
-	enum dataTypes {TYPE_BYTE = 1, TYPE_ASCII, TYPE_SHORT, TYPE_LONG,
-			TYPE_RATIONAL};
+// struct tag * generate_tiff_tags(unsigned int width, unsigned int height){
+// 	enum dataTypes {TYPE_BYTE = 1, TYPE_ASCII, TYPE_SHORT, TYPE_LONG,
+// 			TYPE_RATIONAL};
 
-	struct tag *tags = malloc(sizeof(struct tag) * NUM_TAGS); 
+// 	struct tag *tags = (struct tag[11]){
+// 		{TAG_WIDTH,             TYPE_LONG,  1, width}, 
+// 		{TAG_HEIGHT,            TYPE_LONG,  1, height},
+// 		{TAG_BITS_PER_SAMPLE,   TYPE_SHORT, 1, 8},
+// 		{TAG_COMPRESSION,       TYPE_SHORT, 1, 1},
+// 		{TAG_INTERPRETATION,    TYPE_SHORT, 1, 1},
+// 		{TAG_STRIP_OFFSETS,     TYPE_LONG,  1, 8},
+// 		{TAG_ROWS_PER_STRIP,    TYPE_SHORT, 1, height},
+// 		{TAG_STRIP_BYTE_COUNTS, TYPE_LONG,  1, height * width},
+// 		{TAG_X_RESOLUTION,      TYPE_LONG,  1, 1},
+// 		{TAG_Y_RESOLUTION,      TYPE_LONG,  1, 1},
+// 		{TAG_RESOLUTION_UNIT,   TYPE_SHORT, 1, 1}
+// 	};
 
-	tags = (struct tag[NUM_TAGS]){
-		{TAG_WIDTH,             TYPE_LONG,  1, width}, 
-		{TAG_HEIGHT,            TYPE_LONG,  1, height},
-		{TAG_BITS_PER_SAMPLE,   TYPE_SHORT, 1, 8},
-		{TAG_COMPRESSION,       TYPE_SHORT, 1, 1},
-		{TAG_INTERPRETATION,    TYPE_SHORT, 1, 1},
-		{TAG_STRIP_OFFSETS,     TYPE_LONG,  1, 8},
-		{TAG_ROWS_PER_STRIP,    TYPE_SHORT, 1, height},
-		{TAG_STRIP_BYTE_COUNTS, TYPE_LONG,  1, height * width},
-		{TAG_X_RESOLUTION,      TYPE_LONG,  1, 1},
-		{TAG_Y_RESOLUTION,      TYPE_LONG,  1, 1},
-		{TAG_RESOLUTION_UNIT,   TYPE_SHORT, 1, 1}
-	};
+// 	return tags;
+// }
 
-	return tags;
+void write_file(unsigned char *buffer, unsigned int array, size_t fileLength){
+	char fileName[10];
+	sprintf(fileName, "%d.tiff", array);
+	FILE *file = fopen(fileName, "wb");
+	fwrite(buffer, sizeof(unsigned char), fileLength, file);
+	fclose(file); 
 }
 
-void generate_tiff_file(struct tag header, struct tag *tags,
+void generate_tiff_file(struct header header, struct tag tags[11],
 	 	unsigned char *pixelData, unsigned int arraySize, unsigned int array){
-	size_t fileLength = sizeof(header) +
+	uint16_t numTags = NUM_TAGS;
+	uint32_t tiffEnd = TIFF_END;
+	size_t fileLength = sizeof(struct header) +
 	                    arraySize +
-	                    NUM_TAGS * sizeof(struct tag) +
-	                    sizeof(uint16_t) +
-	                    sizeof(uint32_t);
-	unsigned char *buffer = (char *)malloc()
+	                    numTags * sizeof(struct tag) +
+	                    sizeof(numTags)+
+	                    sizeof(tiffEnd);
+	
+	unsigned char *buffer = (unsigned char *)malloc(sizeof(unsigned char) * 
+			fileLength);
+	unsigned char *marker = buffer;
+
+	printf("\n%d", arraySize);
+	
+	memcpy(marker, &header, sizeof(struct header));
+	marker += sizeof(struct header);
+
+	memcpy(marker, pixelData, arraySize);
+	marker += arraySize;
+
+	memcpy(marker, &numTags, sizeof(numTags));
+	marker += sizeof(numTags);
+	printf("%x\n", numTags);
+
+	int i;
+	for(i = 0; i < numTags; ++i){
+		memcpy(marker, &tags[i], sizeof(struct tag));
+		marker += sizeof(struct tag);
+	}
+
+	memcpy(marker, &tiffEnd, sizeof(tiffEnd));
+
+	printf("\n%d", fileLength);
+
+	write_file(buffer, array, fileLength);
 }
 
 int main(int argc, char **argv){
@@ -156,7 +192,9 @@ int main(int argc, char **argv){
 
 	//set a pointer the correct set of pixel data 
 	set_buffer_to_pixel_array(&inputBuffer, &pixelDataBuffer, 0, arraySize);
-	
+	printf("%d\n", numRows);
+	printf("%d\n", numCols);
+
 	//confirm values
 	printf("%d\n", numImages);
 	int i;
@@ -165,9 +203,28 @@ int main(int argc, char **argv){
 	  printf("%d ", testInt);	
 	}
 
+	unsigned int width = numCols;
+	unsigned int height = numRows;
+
+	enum dataTypes {TYPE_BYTE = 1, TYPE_ASCII, TYPE_SHORT, TYPE_LONG,
+			TYPE_RATIONAL};
+
 	struct header tiffHeader = generate_tiff_header(numCols, numRows);
-	struct tag *tiffTags = generate_tiff_tags(numCols, numRows);
-	
+	struct tag tiffTags[11] = {
+		{TAG_WIDTH,             TYPE_LONG,  1, width}, 
+		{TAG_HEIGHT,            TYPE_LONG,  1, height},
+		{TAG_BITS_PER_SAMPLE,   TYPE_SHORT, 1, 8},
+		{TAG_COMPRESSION,       TYPE_SHORT, 1, 1},
+		{TAG_INTERPRETATION,    TYPE_SHORT, 1, 0},
+		{TAG_STRIP_OFFSETS,     TYPE_LONG,  1, 8},
+		{TAG_ROWS_PER_STRIP,    TYPE_SHORT, 1, height},
+		{TAG_STRIP_BYTE_COUNTS, TYPE_LONG,  1, height * width},
+		{TAG_X_RESOLUTION,      TYPE_LONG,  1, 1},
+		{TAG_Y_RESOLUTION,      TYPE_LONG,  1, 1},
+		{TAG_RESOLUTION_UNIT,   TYPE_SHORT, 1, 1}
+	};
+
+	generate_tiff_file(tiffHeader, tiffTags, pixelDataBuffer, arraySize, 0);
 
 	return 0;
 	
