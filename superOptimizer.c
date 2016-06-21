f79d8c9644ddc1932f05f92914fbb0c6481ada54
Kyle and Robert <rrderber@gmail.com>
2016-06-21 15:16:48 -0500

    made a codonOptimizer that maps groups

--- superOptimizer.c
+++ superOptimizer.c
@@ -24,7 +24,7 @@ char operator(char input, enum operationID opID, int val){
 			break;
 	
 		case and:
-			input = input & val;
+			input = input & ~val;
 			break;	
 
 		case xor:
@@ -49,7 +49,7 @@ int compare (char* input, int inputSize, char * startingInput){
 	for(i=0; i < inputSize; ++i){
 		for(j=0; j < inputSize; ++j){
 			if(i == input[j]){
-				++matches;		
+				++matches;
 				continue;
 			}
 		}
@@ -70,14 +70,21 @@ int superOptimizer (char * startingInput, char * input, int inputSize, int totOp
 	int opsSize = sizeof(operations)/sizeof(char);
 	int minInput = findmin(input,inputSize);
 	//printf("%s%d\n", "minInput: ", minInput);
-	int opsMax[] = {128, minInput+1, 128, 128, 16, 16};
+	int opsMax[] = {128, minInput+1, 128, 128, 8, 8};
 	for(k = 0; k < opsSize; ++k){
+		int opIt = totOps - numOps;
 		int constMax = opsMax[k];
+		if(opIt > 0 && (opsSeq[opIt -1] == operations[k] ||
+			 (k==0 && opsSeq[opIt-1] == operations[1]) ||
+				(k==1 && opsSeq[opIt-1] == operations[0]) ||
+					(k==4 && opsSeq[opIt-1] == operations[5]) ||
+						(k==5 && opsSeq[opIt -1] == operations[4])))
+							continue;
 		for(i = 0; i < constMax; ++i){
 			for(j = 0; j < inputSize; ++j){				
 				newinput[j] = operator(input[j],k,i);
-				opsSeq[totOps -  numOps] = operations[k];
-				numSeq[totOps - numOps] = i;
+				opsSeq[opIt] = operations[k];
+				numSeq[opIt] = i;
 			}
 		if(!compare(newinput, inputSize, startingInput)) return 0;
 		else if(numOps > 1 &&
@@ -89,7 +96,8 @@ int superOptimizer (char * startingInput, char * input, int inputSize, int totOp
 }
 
 int main(int argc, char** argv){
-	char input[] = {'\x00','\x01','\x02','\x03','\x04'};
+//	char input[] = {'\x00','\x01','\x02','\x03','\x04'};
+	char input[] = {'A','C','G','T'};
 	int inputSize = sizeof(input)/sizeof(char);
 	int maxNumOps = 6;
 	char opsSeq [maxNumOps];
@@ -116,6 +124,7 @@ int main(int argc, char** argv){
 		for(i=0;i < numOps; ++i){
 			printf("%d%c ",i,':');
 			int opit = maxNumOps - numOps + i;
+			if(opsSeq[opit] == '&') numSeq[opit] = ~numSeq[opit];
 			printf("%c %d",opsSeq[opit], numSeq[opit]);
 			printf("\n");
 		}
