61bf35d8d4132d30585ab854969e781ff4c35a58
Kyle and Robert <rrderber@gmail.com>
2016-06-28 16:54:39 -0500

    randFileGen now supports Multi-Fasta creation.

--- DNA Sequence Transformations/randFileGen.c
+++ DNA Sequence Transformations/randFileGen.c
@@ -3,6 +3,8 @@
 *
 *Generates random DNA sequence files for testing
 *
+* Input: [char fileType] [int fileSize (in bytes)] [name of file to be created]
+*
 */
 
 #include <stdio.h>
@@ -10,12 +12,16 @@
 #include <string.h>
 #include <time.h>
 
-int randGen (char* fileType, int fileSize, char* fileName) {
+int randGen (char* fileType, int fileSize, char* fileName, int numSeq) {
 	int i;
-	srand((unsigned int)time(NULL)); 
+	time_t currentTime;
+	srand((unsigned int)time(&currentTime)); 
 	if(!strcmp(fileType,"f")||!strcmp(fileType,"F")){ //FASTA
 		FILE * ofp;
 		if((ofp = fopen(fileName,"w"))!= NULL){
+		char dateAndTime [80]; 			//Create Header Line
+		strftime(dateAndTime, 80, "%x - %I:%M%p", localtime(&currentTime));
+		fprintf(ofp,"%s %s %s %s\n","> Randomly Generated File on", dateAndTime, "File Name:",fileName);
 		for(i=0; i < fileSize; ++i){
 			int letter = rand()%4;
 			if(letter == 0){			
@@ -27,15 +33,72 @@ int randGen (char* fileType, int fileSize, char* fileName) {
 			}else if(letter == 3){			
 				fprintf(ofp,"%c",'T');
 			}
+			if(i%70 == 69){			// Newline every 70 characters (custom)
+				fprintf(ofp,"%c",'\n');
+			}
 		}
 		
 		fclose(ofp);
 		printf("\"%s\" %s\n",fileName,"FASTA file created.");
 		}else {
-			printf("Error writing File");	
+			printf("Error writing file");	
 		}
 	}else if(!strcmp(fileType,"m")||!strcmp(fileType,"M")){ // Multi-FASTA
-		printf("Sorry, this feature is currently unavailble.");
+		int j;
+		FILE * ofp;
+		if((ofp = fopen(fileName,"w"))!= NULL){
+			char dateAndTime [80]; 			//Create Header Line
+			strftime(dateAndTime, 80, "%x - %I:%M%p", localtime(&currentTime));
+			fprintf(ofp,"%s %s %s %s\n",">Randomly Generated File on", dateAndTime, "File Name:",fileName);		
+			int seqSize = fileSize/numSeq;
+			int remainder = fileSize%numSeq;
+	
+			for(i=0; i<numSeq; i++){
+				if(i!= 0){
+					fprintf(ofp,"\n%s %d\n",">Sequence Header #",(i+1));
+				}
+				if(i != numSeq-1){
+					for(j=0; j < seqSize; ++j){
+						int letter = rand()%4;
+						if(letter == 0){			
+							fprintf(ofp,"%c",'A');
+						}else if(letter == 1){			
+							fprintf(ofp,"%c",'C');
+						}else if(letter == 2){			
+							fprintf(ofp,"%c",'G');
+						}else if(letter == 3){			
+							fprintf(ofp,"%c",'T');
+						}
+						if(j%70 == 69){			// Newline every 70 characters (custom)
+							fprintf(ofp,"%c",'\n');
+						}
+					}
+				}
+				else{					// Attatch remainder to the last sequence 
+					for(j=0; j < seqSize+remainder; ++j){
+						int letter = rand()%4;
+						if(letter == 0){			
+							fprintf(ofp,"%c",'A');
+						}else if(letter == 1){			
+							fprintf(ofp,"%c",'C');
+						}else if(letter == 2){			
+							fprintf(ofp,"%c",'G');
+						}else if(letter == 3){			
+							fprintf(ofp,"%c",'T');
+						}
+						if(j%70 == 69){			// Newline every 70 characters (custom)
+							fprintf(ofp,"%c",'\n');
+						}
+					}
+				}
+			}
+	
+			fclose(ofp);
+			printf("\"%s\" %s\n",fileName,"Multi-FASTA file created.");	
+			
+		}else{		
+			printf("Error writing file");
+		}
 	}else if(!strcmp(fileType,"q")||!strcmp(fileType,"Q")){ // FASTQ
 		printf("Sorry, this feature is currently unavailble.");
 	}else if(!strcmp(fileType,"s")||!strcmp(fileType,"S")){ // SAM
@@ -54,7 +117,11 @@ int randGen (char* fileType, int fileSize, char* fileName) {
 int main (int argc, char ** argv){ // [char fileType] [int fileSize (in bytes)] [FileName]
 
 	if(argc == 4){
-		randGen(argv[1], atoi(argv[2]), argv[3]);
+		randGen(argv[1], atoi(argv[2]), argv[3], 1);
+		return 0;
+	}
+	else if (argc == 5){
+		randGen(argv[1], atoi(argv[2]), argv[3], atoi(argv[4]));
 		return 0;
 	}
 	else{
