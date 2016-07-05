bf48acf8d0cf51acc8c31e10e06bfec5128260ef
Jason <jasondzheng@gmail.com>
2016-07-05 14:54:19 +0000

    .

--- Character Trajectories/Tests.c
+++ Character Trajectories/Tests.c
@@ -2,27 +2,14 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
-
-int sort_compare(const void * a, const void * b) {
-  return ( *(int*)a - *(int*)b );
-}
-
-int calc_avg_k_lowest_runs(int **array, int numRums, int k) {
-	qsort(*array, numRums, sizeof(int), sort_compare);
-	int i;
-	for (i = 0; i < numRums; ++i) {
-		printf("%d\n", *(*array + i));
-
-	}
-}
+#include <sys/utsname.h>
 
 int main(){
-  int *array = malloc(5 * sizeof(int)); 
-  int i;
-  for (i = 0; i < 5; i++) {
-  	array[i] = 5 - i;
-  }
-  calc_avg_k_lowest_runs(&array, 5, 3);
+
+    struct utsname system;
+    printf("SystemName: %s\n", system.sysname);
+    printf("Release: %s\n", system.release);
+    printf("Machine: %s\n", system.machine);
 
 	
 }
