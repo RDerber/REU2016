3c2ce0a1605f04507a7e0310840edcc3c262467d
Jason <jasondzheng@gmail.com>
2016-07-08 18:00:58 +0000

    directories organized

--- Character Trajectories/Tests.c
+++ Character Trajectories/Tests.c
@@ -2,14 +2,8 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
-#include <sys/utsname.h>
+#include <regex.h>
 
 int main(){
-
-    struct utsname system;
-    printf("SystemName: %s\n", system.sysname);
-    printf("Release: %s\n", system.release);
-    printf("Machine: %s\n", system.machine);
-
-	
+	printf("%d", 1);
 }
