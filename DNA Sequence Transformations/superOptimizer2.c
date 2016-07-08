1f3132cf43d5ae357209f667bd234b3b556307e7
summer16 <summer16@roger-su16c.seas.wustl.edu>
2016-07-08 14:19:26 -0500

    changed superOptimizer to use functionptrs,store all data, and evaluate inputs

--- DNA Sequence Transformations/superOptimizer2.c
+++ DNA Sequence Transformations/superOptimizer2.c
@@ -6,52 +6,56 @@
 #include "writeJson.h"
 
 enum operationID {add = 0, sub = 1, and = 2, xor = 3, lsl = 4, lsr = 5};
+char operations[] = {'+','-','&','^','<','>'};
 
-int findmin(char* input, int inputSize){
+unsigned char addFunc(unsigned char a, unsigned int b){
+	return a+b;
+}
+
+unsigned char andFunc(unsigned char a, unsigned int b){
+	return a&(~b);
+}
+
+unsigned char xorFunc(unsigned char a, unsigned int b){
+	return a^b;
+}
+
+unsigned char shiftLeftFunc(unsigned char a, unsigned int b){
+	unsigned char ans = a << b;
+	return ans;
+}
+
+unsigned char shiftRightFunc(unsigned char a, unsigned int b){
+	unsigned char ans = a >> b;	
+	return ans; 
+}
+
+unsigned char assignFunc(unsigned char a, unsigned int b){
+	return b;
+}
+
+unsigned char (*functionPtrs[])(unsigned char,unsigned int) = {&addFunc,&andFunc,&xorFunc,&shiftLeftFunc,&shiftRightFunc,&assignFunc};
+
+int findmin(char* input, int numDistInputs){
 	int min = input[0];
 	int i;
-	for(i = 1; i<inputSize; ++i){
+	for(i = 1; i<numDistInputs; ++i){
 		if(input[i] < min) min = input[i];
 	}
 	return min;
 }
 
 char operator(char input, enum operationID opID, int val){
-
-	switch(opID){
-		case add:
-			input = input + val;
-			break;
-
-		case sub:
-			input = input - val;
-			break;
-	
-		case and:
-			input = input & ~val;
-			break;	
-
-		case xor:
-			input = input ^ val;
-			break;
-
-		case lsl:
-			input = input << val;
-			break;
-
-		case lsr:
-			input = input >> val;
-			break;
-	}
+	input = functionPtrs[opID](input,val);
 	return input;
 }
 
-int compare (char* input, int inputSize, char * startingInput){
+int compare (char* input, int numDistInputs, char * startingInput){
 	int i;
 	int j;
 	int matches = 0;
-	for(i=0; i < inputSize; ++i){
-		for(j=0; j < inputSize; ++j){
+	for(i=0; i < numDistInputs; ++i){
+		for(j=0; j < numDistInputs; ++j){
 			if(i == input[j]){
 				++matches;
 				continue;
@@ -59,20 +63,19 @@ int compare (char* input, int inputSize, char * startingInput){
 		}
 		if(matches != i+1) return -1;
 	}
-	if(matches == inputSize){
+	if(matches == numDistInputs){
 		printf("Transformations:\nInput:\tOutput:\n");
-		for(i=0;i<inputSize;++i)
+		for(i=0;i<numDistInputs;++i)
 			printf("%d%s%d\n",startingInput[i],"   ->   ", input[i]);
 		return 0;
 	}
 }
 
-int superOptimizer (char * startingInput, char * input, int inputSize, int totOps, int numOps, char* opsSeq, int* numSeq){
+int superOptimizer (char * startingInput, char * input, int numDistInputs, int totOps, int numOps, char* opsSeq, int* numSeq){
 	int i,j,k;
-	char newinput[inputSize];
-	char operations[] = {'+','-','&','^','<','>'};
+	char newinput[numDistInputs];
 	int opsSize = sizeof(operations)/sizeof(char);
-	int minInput = findmin(input,inputSize);
+	int minInput = findmin(input,numDistInputs);
 	//printf("%s%d\n", "minInput: ", minInput);
 	int opsMax[] = {128, minInput+1, 128, 128, 8, 8};
 	for(k = 0; k < opsSize; ++k){
@@ -85,57 +88,62 @@ int superOptimizer (char * startingInput, char * input, int inputSize, int totOp
 //						(k==5 && opsSeq[opIt -1] == operations[4])))
 //							continue;
 		for(i = 0; i < constMax; ++i){
-			for(j = 0; j < inputSize; ++j){				
+			for(j = 0; j < numDistInputs; ++j){
 				newinput[j] = operator(input[j],k,i);
-				opsSeq[opIt] = operations[k];
+				opsSeq[opIt] = k;
 				numSeq[opIt] = i;
 			}
-		if(!compare(newinput, inputSize, startingInput)) return 0;
+		if(!compare(newinput, numDistInputs, startingInput)) return 0;
 		else if(numOps > 1 &&
-			!superOptimizer(startingInput, newinput, inputSize, totOps, numOps - 1, opsSeq, numSeq))
+			!superOptimizer(startingInput, newinput, numDistInputs, totOps, numOps - 1, opsSeq, numSeq))
 				return 0;
 		}
 	}
 	return -1;
 }
 
-// MAKE EVALUATE FUNCTION
 
-int main(int argc, char** argv){	//[inputSize](for random inputs) or specify inputs and files to transform [input 1]...[input n][evaluateFile][outputFile]
+char evaluate(int input, char * opsSeq, int * numSeq, int maxNumOps){
+	int i,j;
+	char output;
+	for(i=0;i<maxNumOps;++i)
+		output = operator(input, opsSeq[i], numSeq[i]);
+	return output;
+}
+
+int main(int argc, char** argv){	//[number of Distinct Inputs][file size](for random inputs and file size) or specify inputs and files to transform [input 1]...[input n][evaluateFile][outputFile]
 //	char input[] = {'\x00','\x01','\x02','\x03','\x04'};
 //	char input[] = {'A','C','G','T','N'};
 	int i,j,k;
-	int maxInputSize = 200;	//input size exceeding 200 causes our naive unique number generator to be extremely slow, exceeding 256
+	int maxNumDistInputs = 200;	//input size exceeding 200 causes our naive unique number generator to be extremely slow, exceeding 256
 				//breaks the number generator
-				
-	int numInputSets = 10; 
+
 	int runs = 10;
 	int numDataForms = 6; //RunTime, EvalTime, OpArray, numArray,input, output
 	
 	char * input;
 	char * output;
-	int inputSize;
+	int numDistInputs;
 	double runTimes[runs];
 	double evalTimes[runs];
 	int maxNumOps = 6;
+	long fileSize = atoi(argv[2]);
 	
 	if(argc != 3){
-		printf("Please provide an inputSize and a fileSize");
+		printf("Please provide an numDistInputs and a fileSize");
 		return -1;
 	}
-	if(atoi(argv[1]) >= maxInputSize){
-		printf("inputSize too large for unique random number generator");
+	if(atoi(argv[1]) >= maxNumDistInputs){
+		printf("numDistInputs too large for unique random number generator");
 		return -1;
 	}
-	
-	runTimes = malloc(runs *sizeof(double)); 
-	evalTimes = malloc(runs *sizeof(double)); 
-	inputSize = atoi(argv[1]);
-	input = calloc(sizeof(char),inputSize);
-	output = calloc(sizeof(char),inputSize);
+	 
+	numDistInputs = atoi(argv[1]);
+	input = calloc(sizeof(char),numDistInputs);
+	output = calloc(sizeof(char),numDistInputs);
 	
 	srand((unsigned int)time(0));
-	for(i=0;i<inputSize;++i){
+	for(i=0;i<numDistInputs;++i){
 		input[i] = rand()%255;
 		for(j=0;j<i;++j){
 			if(input[i] == input[j]) --i;
@@ -144,37 +152,52 @@ int main(int argc, char** argv){	//[inputSize](for random inputs) or specify inp
 		
 	
 	char opsSeq [maxNumOps];
+	char opRep [maxNumOps];
 	int numSeq [maxNumOps] ;
 	for(i = 0; i < maxNumOps; ++i){
 		opsSeq[i] = ' ';
 		numSeq[i] = 0;
 	}
-		
+	char * testBuff = malloc(fileSize*sizeof(char));
+	srand(time(0));
+	for(i=0;i<fileSize;++i){
+		testBuff[i] = input[rand()%numDistInputs];
+	}
 	int success = -1;
 	struct timeval time0,time1;
 	for(i=0;i<runs;i++){
 		gettimeofday(&time0,NULL);
-		success = superOptimizer(input, input, inputSize, maxNumOps, maxNumOps, opsSeq, numSeq);
+		success = superOptimizer(input, input, numDistInputs, maxNumOps, maxNumOps, opsSeq, numSeq);
 		gettimeofday(&time1,NULL);
 		double runTime = (time1.tv_sec-time0.tv_sec)*1000000LL + time1.tv_usec - time0.tv_usec;
 		runTimes[i] = runTime;
 	}
 	
+	for(i=0;i<maxNumOps;++i){
+		if(numSeq[i] != 0){
+			opRep[i] = operations[opsSeq[i]];
+		}else{
+			opRep[i] = ' ';
+		}
+	}
+	
 	for(i=0;i<runs;i++){
 		gettimeofday(&time0,NULL);
-		success = evaluate(input, input, inputSize, maxNumOps, maxNumOps, opsSeq, numSeq);
+		for(j = 0; j < fileSize; ++j){
+			evaluate(input[j], opsSeq, numSeq, maxNumOps);
+		}
 		gettimeofday(&time1,NULL);
 		double evalTime = (time1.tv_sec-time0.tv_sec)*1000000LL + time1.tv_usec - time0.tv_usec;
 		evalTimes[i] = evalTime;
-		}
 	}
-		
-	for(i=0; i < inputSize; ++i){
-		output[i] = evaluate(input[i],   );
+	
+
+	for(i=0; i < numDistInputs; ++i){
+			output[i] = evaluate(input[i], opsSeq, numSeq,maxNumOps);
 	}
 
 	
-	double **timeArray[numDataForms];
+	double *timeArray[numDataForms];
 	char *labelArray[numDataForms];	
 	timeArray[0] = runTimes;
 	timeArray[1] = evalTimes;
@@ -184,14 +207,12 @@ int main(int argc, char** argv){	//[inputSize](for random inputs) or specify inp
 	
 	int numLabels= sizeof(labelArray); 
 	
-	if(write_super_file(timeArray, labelArray, numLabels, runs, opsSeq, numSeq, maxNumOps, input, output, inputSize) < 0)
-		printf("error writing data file\n"); 
+//	if(write_super_file(timeArray, labelArray, numLabels, runs, opRep, numSeq, maxNumOps, input, output, numDistInputs) < 0)
+//		printf("error writing data file\n"); 
 		
 	
 	free(input);	
 	free(output);                            
-	free(runTimes);
-	free(evalTimes);
 
 //	int numOps = 0;
 //	for(i = 0; i<maxNumOps; ++i){
